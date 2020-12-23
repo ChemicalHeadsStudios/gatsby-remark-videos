3e6bb9cce16bc997779f3d90fe52efb1bcc41e1a
MarkJGx <38701409+MarkJGx@users.noreply.github.com>
2020-12-23 03:33:13 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -105,7 +105,7 @@ module.exports = (
     }
 
     const videoTag = `
-    <video autoplay loop preload style="${videoAspectStyle}" >
+    <video controls="" style="${videoAspectStyle}" >
       ${sourceTags.join('')}
     </video>
     `
